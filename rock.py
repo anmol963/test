187d654c477cd4c1612750a776047d555ac1c319
anmol963 <anmolsingh0098765@gmail.com>
2022-02-10 20:32:12 +0530

    second version of rock

--- rock.py
+++ rock.py
@@ -1,5 +1,9 @@
 a=int(input("Enter the number: "))
 if a%2==0:
-    print("even")
+    print("even",end="")
+    if a>20:
+        print(" and greater than twenty")
+    else:
+        print(" and less than twenty")
 else:
     print("odd")
